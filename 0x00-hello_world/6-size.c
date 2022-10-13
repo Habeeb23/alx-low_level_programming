d1d3448dc35a21f809a3f048fff306c4af1cf82b
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2022-10-14 06:59:38 +0800

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
-#include "unistd.h"
+#include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - Prints the sizeof many var types
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
