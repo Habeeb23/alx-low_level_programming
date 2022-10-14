c80b907cc259807b42043f8b772e1b52885f403b
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2022-10-15 02:19:17 +0800

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,47 +1,24 @@
 #include <stdio.h>
 
-
-
 #include <ctype.h>
-
 /**
- *
- *  * main - main block
- *
- *   * Description: Get a random number and print the number
- *
- *    * positive,negative or zero
- *
- *     * Return: Always 0 (success)
- *
- *      */
-
+ * main - main block
+ * Description: Get a random number and print the number
+ * positive,negative or zero
+ * Return: Always 0 (success)
+ */
 int main(void)
-
 {
+	int letters;
 
-		int letters;
-
-
-
-			for (letters = 'a'; letters <= 'z'; letters++)
-
-					{
-
-								if (letters  == 'q' || letters == 'e')
-
-											{
-
-															continue;
-
-																	}
-
-										putchar(letters);
-
-											}
-
-				putchar('\n');
-
-					return (0);
-
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		if (letters  == 'q' || letters == 'e')
+		{
+			continue;
+		}
+		putchar(letters);
+	}
+	putchar('\n');
+	return (0);
 }
