5c0afadc5af7c26937bb802e1be7a0da44ef4ae1
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2022-10-20 20:01:11 +0800

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,37 +1,19 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- * * _isdigit - A function that checks for a digit
- *
- * * @c: input
- *
- * *
- *
- * * Return: 1 if c is a digit
- *
- * * 0 otherwise
- *
- * */
-
+* _isdigit - A function that checks for a digit
+* @c: input
+*
+* Return: 1 if c is a digit
+* 0 otherwise
+*/
 int _isdigit(int c)
-
 {
-
-		if ((c >= '0') && (c <= '9'))
-
-				{
-
-							return (1);
-
-								}
-
-			else
-
-					{
-
-								return (0);
-
-									}
-
+	if ((c >= '0') && (c <= '9'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
