afa82440ab6587ef29f1c6073698863f634a7934
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2022-10-20 20:09:25 +0800

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,23 +1,12 @@
 #include "main.h"
 
-
-
 /**
- *
- * * mul - A function that multiplies two integers.
- *
- * * @a: First input
- *
- * * @b: Second input
- *
- * * Return: Multiplication result of the 2 input.
- *
- * */
-
+* mul - A function that multiplies two integers.
+* @a: First input
+* @b: Second input
+* Return: Multiplication result of the 2 input.
+*/
 int mul(int a, int b)
-
 {
-
-		return (a * b);
-
+	return (a * b);
 }
