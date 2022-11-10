138e0956200d2c0dd81e636b4c05224744cc8986
Habeeb23 <jimohhabeeb23@gmail.com>
2022-11-09 16:03:15 -0800

    1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,16 +1,23 @@
-#include "main.h"
-/**
- * _memcpy - copies memory area,
- * @dest: destination memory area.
- * @src: source memory area.
- * @n: bytes filled.
- * Return: the pointer to dest.
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
+nclude "main.h"
 
-<<<<<<< HEAD
+/**
+ *
+ *  * _memcpy - copies memory area,
+ *
+ *   * @dest: destination memory area.
+ *
+ *    * @src: source memory area.
+ *
+ *     * @n: bytes filled.
+ *
+ *      * Return: the pointer to dest.
+ *
+ *       */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+
 		unsigned int i;
 
 
@@ -23,11 +30,4 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 				return (dest);
 
-}
-=======
-	for (i = 0; i < n; i++)
-		*(dest + i) =  *(src + i);
->>>>>>> f3458fa02a67d6b33e8c9608d935eaa0e1ddd071
-
-	return (dest);
 }
