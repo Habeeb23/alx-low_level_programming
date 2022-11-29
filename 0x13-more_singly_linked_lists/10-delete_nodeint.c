ade82ffe5f6fe69531a320e4ea1d1c9b2d91cd30
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2022-11-30 02:16:33 +0800

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,51 +1,40 @@
-lude "lists.h"
-
-
+#include "lists.h"
 
 /**
+ * delete_nodeint_at_index - Deletes the node at a given
+ *                           index of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @index: The index of the node to be deleted - indices start at 0.
  *
- *  * pop_listint - Deletes the head node of a listint_t list.
- *
- *   * @head: A pointer to the address of the
- *
- *    *        head of the listint_t list.
- *
- *     *
- *
- *      * Return: If the linked list is empty - 0.
- *
- *       *         Otherwise - The head node's data (n).
- *
- *        */
-
-int pop_listint(listint_t **head)
-
+ * Return: On success - 1.
+ *         On failure - -1.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *tmp, *copy = *head;
+	unsigned int node;
 
-		listint_t *tmp;
+	if (copy == NULL)
+		return (-1);
 
-			int ret;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(copy);
+		return (1);
+	}
 
+	for (node = 0; node < (index - 1); node++)
+	{
+		if (copy->next == NULL)
+			return (-1);
 
+		copy = copy->next;
+	}
 
-				if (*head == NULL)
-
-							return (0);
-
-
-
-					tmp = *head;
-
-						ret = (*head)->n;
-
-							*head = (*head)->next;
-
-
-
-								free(tmp);
-
-
-
-									return (ret);
-
+	tmp = copy->next;
+	copy->next = tmp->next;
+	free(tmp);
+	return (1);
 }
