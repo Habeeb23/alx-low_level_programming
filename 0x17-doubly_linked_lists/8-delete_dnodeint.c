7ae73355d8004e555238f8145b901af9934a7237
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-01-13 23:46:32 +0800

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,73 +1,39 @@
 #include "lists.h"
 
-
-
 /**
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t
+ *                            at a given index.
+ * @head: A pointer to the head of the dlistint_t.
+ * @index: The index of the node to delete.
  *
- *  * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
- *
- *   *                            list at a given position.
- *
- *    * @h: A pointer to the head of the dlistint_t list.
- *
- *     * @idx: The position to insert the new node.
- *
- *      * @n: The integer for the new node to contain.
- *
- *       *
- *
- *        * Return: If the function fails - NULL.
- *
- *         *         Otherwise - the address of the new node.
- *
- *          */
+ * Return: Upon success - 1.
+ *         Otherwise - -1.
+ */
 
-
-
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *tmp = *head;
 
-		dlistint_t *tmp = *h, *new;
-
-
-
-			if (idx == 0)
-
-						return (add_dnodeint(h, n));
-
-				for (; idx != 1; idx--)
-
-						{
-
-									tmp = tmp->next;
-
-											if (tmp == NULL)
-
-															return (NULL);
-
-												}
-
-					if (tmp->next == NULL)
-
-								return (add_dnodeint_end(h, n));
-
-						new = malloc(sizeof(dlistint_t));
-
-							if (new == NULL)
-
-										return (NULL);
-
-								new->n = n;
-
-									new->prev = tmp;
-
-										new->next = tmp->next;
-
-											tmp->next->prev = new;
-
-												tmp->next = new;
-
-
-
-													return (new);
+	if (*head == NULL)
+		return (-1);
+	for (; index != 0; index--)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
+	free(tmp);
+	return (1);
+}
