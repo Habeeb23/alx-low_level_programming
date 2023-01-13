950f71ddf4e3c2faffb77bfddcd211d36ddde4b0
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-01-13 23:44:28 +0800

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,41 +1,20 @@
 #include "lists.h"
 
-
-
 /**
+ * sum_dlistint - Sums all the data of a dlistint_t list.
+ * @head: The head of the dlistint_t list.
  *
- *  * print_dlistint - Prints all the elements of a dlistint_t list.
- *
- *   * @h: The head of the dlistint_t list.
- *
- *    *
- *
- *     * Return: The number of nodes in the list.
- *
- *      */
+ * Return: The sum of all the data.
+ */
 
-
-
-size_t print_dlistint(const dlistint_t *h)
-
+int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
 
-		size_t nodes = 0;
-
-
-
-			while (h)
-
-					{
-
-								nodes++;
-
-										printf("%d\n", h->n);
-
-												h = h->next;
-
-													}
-
-				return (nodes);
-
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
