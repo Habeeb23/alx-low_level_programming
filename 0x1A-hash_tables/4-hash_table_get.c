e2a3657a5b5e425ee439f479d107accb295dd1b4
Habeeb23 <113600396+Habeeb23@users.noreply.github.com>
2023-02-07 06:17:55 +0800

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,53 +1,30 @@
+
 #include "hash_tables.h"
 
-
-
 /**
+ * hash_table_get - Retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
  *
- *  * hash_table_create - Creates a hash table.
- *
- *   * @size: The size of the array.
- *
- *    *
- *
- *     * Return: If an error occurs - NULL.
- *
- *      *         Otherwise - a pointer to the new hash table.
- *
- *       */
-
-hash_table_t *hash_table_create(unsigned long int size)
-
+ * Return: If the key cannot be matched - NULL.
+ *         Otherwise - the value associated with key in ht.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *node;
+	unsigned long int index;
 
-		hash_table_t *ht;
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-			unsigned long int i;
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 
-
-				ht = malloc(sizeof(hash_table_t));
-
-					if (ht == NULL)
-
-								return (NULL);
-
-
-
-						ht->size = size;
-
-							ht->array = malloc(sizeof(hash_node_t *) * size);
-
-								if (ht->array == NULL)
-
-											return (NULL);
-
-									for (i = 0; i < size; i++)
-
-												ht->array[i] = NULL;
-
-
-
-										return (ht);
-
+	return ((node == NULL) ? NULL : node->value);
 }
